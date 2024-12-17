fe994de46bc713bb3156adfffce9c8e23dedc4f1
Jayce <97198910+jasjs1@users.noreply.github.com>
2024-12-16 20:47:32 -0600

    Create age_calculator.py

---
+++ easy/age_calculator.py
@@ -0,0 +1,5 @@
+current_year = 2024 # Change to current year if outdated.
+birthyear = int(input("Your birth year: "))
+calculate_age = current_year - birthyear
+
+print(f"You are {calculate_age} years old.")
